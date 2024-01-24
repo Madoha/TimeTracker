aa30f8a2a929d03627f3aa4c3d50562cf061d493
Madoha <madoha00@gmail.com>
2024-01-24 23:51:10 +0600

    add Scaffolded, PostgresSql, Identity

---
+++ TimeTracker/Data/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace TimeTracker.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.Entity<IdentityRole>().HasData(
+                new IdentityRole() { Name="Admin", NormalizedName="ADMIN", ConcurrencyStamp="1" }
+                );
+        }
+    }
+}
